be832140b30d25425380965a546b6f682f346638
Kirill Loktevich <lokkirill@gmail.com>
2020-02-18 17:10:01 +0300

    Did a lot, configured much more

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,50 @@
-var webpack = require("webpack"),
-    path = require("path"),
-    fileSystem = require("fs"),
-    env = require("./utils/env"),
-    CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin,
-    CopyWebpackPlugin = require("copy-webpack-plugin"),
-    HtmlWebpackPlugin = require("html-webpack-plugin"),
-    WriteFilePlugin = require("write-file-webpack-plugin");
+const webpack = require("webpack"),
+  path = require("path"),
+  CleanWebpackPlugin = require("clean-webpack-plugin"),
+  CopyWebpackPlugin = require("copy-webpack-plugin"),
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  WriteFilePlugin = require("write-file-webpack-plugin");
 
-// load the secrets
-var alias = {};
+var fileExtensions = [
+  "jpg",
+  "jpeg",
+  "png",
+  "gif",
+  "eot",
+  "otf",
+  "svg",
+  "ttf",
+  "woff",
+  "woff2"
+];
 
-var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
-
-var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
-
-if (fileSystem.existsSync(secretsPath)) {
-  alias["secrets"] = secretsPath;
-}
-
-var options = {
-  mode: process.env.NODE_ENV || "development",
+const manifestEnvironment = process.env.NODE_ENV || "staging";
+var environment = {
+  mode:
+    process.env.NODE_ENV === "staging" ? "development" : process.env.NODE_ENV,
   entry: {
+    index: path.join(__dirname, "src", "js", "index.js"),
     popup: path.join(__dirname, "src", "js", "popup.js"),
-    options: path.join(__dirname, "src", "js", "options.js"),
-    background: path.join(__dirname, "src", "js", "background.js")
+    background: path.join(__dirname, "src", "js", "background.js"),
+    content: path.join(__dirname, "src", "js", "content.js"),
   },
   output: {
     path: path.join(__dirname, "build"),
-    filename: "[name].bundle.js"
+    filename: "[name].js"
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        loader: "style-loader!css-loader",
-        exclude: /node_modules/
+        test: /\.s?css$/,
+        use: [
+          //MiniCssExtractPlugin.loader,
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
       },
       {
-        test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
+        test: new RegExp(".(" + fileExtensions.join("|") + ")$"),
         loader: "file-loader?name=[name].[ext]",
         exclude: /node_modules/
       },
@@ -45,49 +52,62 @@ var options = {
         test: /\.html$/,
         loader: "html-loader",
         exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
       }
     ]
   },
   resolve: {
-    alias: alias
+    extensions: fileExtensions
+      .map(extension => "." + extension)
+      .concat([".jsx", ".js", ".css"])
   },
   plugins: [
     // clean the build folder
-    new CleanWebpackPlugin(),
-    // expose and write the allowed env vars on the compiled bundle
-    new webpack.EnvironmentPlugin(["NODE_ENV"]),
-    new CopyWebpackPlugin([{
-      from: "src/manifest.json",
-      transform: function (content, path) {
-        // generates the manifest file using the package.json informations
-        return Buffer.from(JSON.stringify({
-          description: process.env.npm_package_description,
-          version: process.env.npm_package_version,
-          ...JSON.parse(content.toString())
-        }))
+    // new CleanWebpackPlugin(["build"]),
+    new CopyWebpackPlugin([
+      {
+        from: `src/manifest_${manifestEnvironment}.json`,
+        to: "manifest.json",
+        toType: "file",
+        transform: function(content, path) {
+          // generates the manifest file using the package.json informations
+          return Buffer.from(
+            JSON.stringify({
+              description: process.env.npm_package_description,
+              version: process.env.npm_package_version,
+              ...JSON.parse(content.toString())
+            })
+          );
+        }
       }
-    }]),
+    ]),
+    new CopyWebpackPlugin([
+      {
+        from: `src/img/${manifestEnvironment}`,
+        toType: "dir"
+      }
+    ]),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "popup.html"),
+      template:
+        process.env.NODE_ENV === "production"
+          ? path.join(__dirname, "src", "popup.html.production")
+          : path.join(__dirname, "src", "popup.html"),
       filename: "popup.html",
       chunks: ["popup"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "options.html"),
-      filename: "options.html",
-      chunks: ["options"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "background.html"),
-      filename: "background.html",
-      chunks: ["background"]
     }),
     new WriteFilePlugin()
   ]
 };
 
-if (env.NODE_ENV === "development") {
-  options.devtool = "cheap-module-eval-source-map";
+if (
+  process.env.NODE_ENV === "development" ||
+  process.env.NODE_ENV === "staging"
+) {
+  environment.devtool = "cheap-module-eval-source-map";
 }
 
-module.exports = options;
+module.exports = environment;
